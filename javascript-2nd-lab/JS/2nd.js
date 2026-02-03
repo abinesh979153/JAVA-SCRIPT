c2d5e8ec852ae86bc67f71e6fdd6da9187f67946
abinesh979153 <abineshabinesh979153@gmail.com>
2026-02-03 19:09:50 +0530

    js-2nd-lab

---
+++ javascript-2nd-lab/JS/2nd.js
@@ -0,0 +1,13 @@
+var name;
+name='abinesh'
+console.log(name)
+name='sathish'
+var since=2007
+console.log(name,since)
+
+// es6
+// let,const
+let employe='rohan'
+let age='18'
+let city='chennai'
+console.log(employe,age,city)
