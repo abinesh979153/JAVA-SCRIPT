67464411c3d016c6598c8bcfef86f6e6b991aa2a
abinesh979153 <abineshabinesh979153@gmail.com>
2026-02-04 14:11:16 +0530

    javascript-2nd-lab

---
+++ javascript-2nd-lab/JS/3rd.js
@@ -0,0 +1,28 @@
+// const x={
+//     x:2007
+// };
+
+// console.log(x)
+
+
+const name='abinesh';
+
+{
+    let name='abi';
+    console.log(name)
+}
+
+const citys=['kolkata','hydrabad'];
+{
+   console.log('inside the block', citys)
+   let city='banglore'
+}
+
+const city=['banglore','chennai'];
+{
+console.log('inside in the block',citys)
+let city='delhi';
+console.log(city)
+}
+
+console.log('outside the block',citys)
