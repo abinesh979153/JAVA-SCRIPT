8fd0ca9c69c5cf39ec35de538b6755cbd4247db7
abinesh979153 <abineshabinesh979153@gmail.com>
2026-02-28 18:24:31 +0530

    js-5th

---
+++ javascript-5th-lab/JS/4th.js
@@ -0,0 +1,31 @@
+console.log('js-4 data types')
+// 1.printivativate data types
+let x=10
+let y=10.34
+console.log(x,typeof(x))
+console.log(y,typeof(y))
+
+var str='guvi f5d bootcamp'
+var str2="guvi fsd bootcamp"
+var str3='guvi fsd bootcamp'
+var str4='"guvi fsd bootcamp'
+console.log(str,typeof(str))
+console.log(str,typeof(str2))
+console.log(str,typeof(str3))
+
+// boolean
+// true/false
+var isActive=true;
+var ispass=false;
+console.log(isActive,ispass,typeof(isActive))
+
+// undefined
+let z;
+console.log(z,typeof(z))
+
+// null
+let b=null;
+console.log(b,typeof(b))
+
+// symbol
+let sym=Symbol('my nam is something')
