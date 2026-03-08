75d345270a95ac9685cdf690004564d62085cb51
abinesh979153 <abineshabinesh979153@gmail.com>
2026-03-08 21:10:30 +0530

    javascript

---
+++ javascript-6th-lab/js/function.js
@@ -0,0 +1,14 @@
+let str="abinesh mern stack"
+console.log(str)
+console.log(str.charAt(3))
+console.log(str.charCodeAt(8))
+console.log(str.charCodeAt())
+console.log(str.charCodeAt(4))
+
+let newstr=str.concat(" course")
+console.log(newstr)
+
+console.log(str.includes("full"))
+console.log(str.includes("aa"))
+console.log(str.includes("F"))
+console.log(str.lastindex("p"))
