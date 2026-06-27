28e874bc1b6b1ca7ba58b76165fff3b122608e32
abinesh979153 <abineshabinesh979153@gmail.com>
2026-06-27 14:55:18 +0530

    javascript

---
+++ javascript-7th-lab/7th.js
@@ -0,0 +1,9 @@
+var rain = false
+
+if(rain)
+{
+    console.log("take an umbrella")
+} 
+else{
+    console.log("enjoy the sunshine")
+}
---
+++ javascript-7th-lab/js/6th.js
@@ -0,0 +1,8 @@
+function  sum(a,b)
+{
+    return( a+b)
+}
+
+  var       total = sum(16770,270)
+  console.log(total)
+
